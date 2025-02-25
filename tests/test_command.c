223c52eb8293a17527aedbc61eb55d316ce14823
Ben Landon <landb0x207@gmail.com>
2025-02-24 22:56:10 -0800

    feat: more tests

--- tests/test_command.c
+++ tests/test_command.c
@@ -1,8 +1,41 @@
 #include "unity.h"
 #include "command.h"
+#include <stdlib.h>
+
+void test_parse_command() {
+    struct command comm;
+    char *src = "ls -la build";
+    int result = parse_command(&comm, &src, 0, 12);
+    char *expected_args[4] = {"ls", "-la", "build", NULL};
+
+    TEST_ASSERT_EQUAL_INT(0, result);
+    TEST_ASSERT_EQUAL_INT(COMMAND_EXEC, comm.type);
+    TEST_ASSERT_EQUAL_STRING_ARRAY(expected_args, comm.args, 4);
+}
+
+void test_parse_builtin_command() {
+    struct command comm;
+    char *src = "cd build";
+    int result = parse_command(&comm, &src, 0, 8);
+    char *expected_args[3] = {"cd", "build", NULL};
+
+    TEST_ASSERT_EQUAL_INT(0, result);
+    TEST_ASSERT_EQUAL_INT(COMMAND_BUILTIN, comm.type);
+    TEST_ASSERT_EQUAL_STRING_ARRAY(expected_args, comm.args, 3);
+}
 
 void test_command_arg_counting() {
     TEST_ASSERT_EQUAL_INT(3, count_command_args("ls -la build", 0, 12));
     TEST_ASSERT_EQUAL_INT(2, count_command_args("ls -la", 0, 6));
     TEST_ASSERT_EQUAL_INT(1, count_command_args("ls", 0, 2));
+}
+
+void test_get_command_args() {
+    char **result = get_command_args("ls -la build", 0, 12);
+    char *expected[3] = {"ls", "-la", "build"};
+
+    TEST_ASSERT_NOT_EQUAL(NULL, result);
+    TEST_ASSERT_EQUAL_STRING_ARRAY(expected, result, 3);
+
+    free(result);
 }
--- tests/test_lang.c
+++ tests/test_lang.c
@@ -1,6 +1,9 @@
 #include "command.h"
 #include "unity.h"
 #include "lang.h"
+#include <stdlib.h>
+#include <string.h>
+#include <strings.h>
 
 void test_parse_line_works() {
     struct lang_node *nodes;
@@ -13,6 +16,13 @@ void test_parse_line_works() {
     TEST_ASSERT_EQUAL_STRING("-la", nodes[0].args[1]);
     TEST_ASSERT_EQUAL_STRING("build", nodes[0].args[2]);
     TEST_ASSERT_EQUAL_STRING(NULL, nodes[0].args[3]);
+}
+
+void test_parse_invalid_input() {
+    struct lang_node *nodes;
+    int result = lang_parse_line(&nodes, "^87@#");
+
+    TEST_ASSERT_EQUAL_INT(-1, result);
 }
 
 void test_node_to_command() {
@@ -29,4 +39,36 @@ void test_node_to_command() {
     TEST_ASSERT_EQUAL_INT(COMMAND_EXEC, comm.type);
     TEST_ASSERT_EQUAL_STRING("ls", comm.exec);
     TEST_ASSERT_EQUAL_STRING_ARRAY(expected_args, comm.args, 4);
+}
+
+void test_node_to_command_no_args() {
+    struct lang_node *nodes;
+    int result = lang_parse_line(&nodes, "ls");
+
+    TEST_ASSERT_EQUAL_INT(1, result);
+
+    struct command comm;
+    char* expected_args[2] = {"ls", NULL};
+    result = lang_parse_command(&nodes[0], &comm);
+
+    TEST_ASSERT_EQUAL_INT(0, result);
+    TEST_ASSERT_EQUAL_INT(COMMAND_EXEC, comm.type);
+    TEST_ASSERT_EQUAL_STRING("ls", comm.exec);
+    TEST_ASSERT_EQUAL_STRING_ARRAY(expected_args, comm.args, 2);
+}
+
+void test_translate_builtin_command() {
+    struct lang_node *nodes;
+    int result = lang_parse_line(&nodes, "cd");
+
+    TEST_ASSERT_EQUAL_INT(1, result);
+
+    struct command comm;
+    char* expected_args[2] = {"cd", NULL};
+    result = lang_parse_command(&nodes[0], &comm);
+
+    TEST_ASSERT_EQUAL_INT(0, result);
+    TEST_ASSERT_EQUAL_INT(COMMAND_BUILTIN, comm.type);
+    TEST_ASSERT_EQUAL_STRING("cd", comm.exec);
+    TEST_ASSERT_EQUAL_STRING_ARRAY(expected_args, comm.args, 2);
 }
